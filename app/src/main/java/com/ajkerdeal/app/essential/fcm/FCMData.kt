2deaaa7f4d2398a6ea89c9ec484559fa4b1e901c
Anup <anupdey99@gmail.com>
2021-03-06 13:36:41 +0600

    gradle updated

--- app/src/main/java/com/ajkerdeal/app/essential/fcm/FCMData.kt
+++ app/src/main/java/com/ajkerdeal/app/essential/fcm/FCMData.kt
@@ -3,7 +3,8 @@ package com.ajkerdeal.app.essential.fcm
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
+
 
 @Parcelize
 data class FCMData(
